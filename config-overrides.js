7ce377bd7715d23ac3efc45dddaf64ccabcffd71
Jacob Dalton <jacob.dalton@sentera.com>
2021-08-16 16:18:46 -0500

    react-map-gl + maplibre + react-rewired

---
+++ config-overrides.js
@@ -0,0 +1,14 @@
+/* config-overrides.js */
+
+module.exports = function override(config, env) {
+  config.module.rules.push({
+    resolve:{
+      alias: {
+        ...config.resolve.alias,
+        'mapbox-gl': 'maplibre-gl'
+      }
+    }
+  })
+
+  return config
+}
